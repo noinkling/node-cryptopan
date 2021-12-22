87e1b1e7343160613e1cb0e417a9c32120d15c61
Malcolm <noinkling@users.noreply.github.com>
2021-12-22 20:38:00 +1300

    Add IPv6 support

--- src/cryptopan.ts
+++ src/cryptopan.ts
@@ -19,7 +19,7 @@ export class CryptoPan {
     const padding = Uint8Array.prototype.slice.call(key, 16, 32);
 
     this.#cipher = crypto.createCipheriv('aes-128-ecb', cipherKey, null);
-    // We use our own padding scheme:
+    // CryptoPAn uses its own padding scheme:
     this.#cipher.setAutoPadding(false);
 
     this.#padding = this.#cipher.update(padding);
@@ -27,7 +27,7 @@ export class CryptoPan {
 
   pseudonymise(ip: Buffer | Uint8Array) {
     if (!(ip instanceof Uint8Array)) {
-      throw new TypeError(`Provided IP address must be a Buffer or Uint8Array`);
+      throw new TypeError(`IP address must be provided as a Buffer or Uint8Array`);
     }
     if (ip.length !== 4 && ip.length !== 16) {
       throw new RangeError(
@@ -40,43 +40,48 @@ export class CryptoPan {
 
   pseudonymiseIPv4(ip: Buffer | Uint8Array) {
     if (!(ip instanceof Uint8Array)) {
-      throw new TypeError(`Provided IP address must be a Buffer or Uint8Array`);
+      throw new TypeError(`IP address must be provided as a Buffer or Uint8Array`);
     }
     if (ip.length !== 4) {
       throw new RangeError(
-        `Provided IP address buffer must be 4 bytes (32 bits) in length, was ${ip.length} bytes`
+        `Provided IPv4 address buffer must be 4 bytes (32 bits) in length, was ${ip.length} bytes`
       );
     }
 
     return this._pseudonymise(ip);
   }
 
-  pseudonymiseIPv6(ip: Buffer | Uint8Array): never {
+  pseudonymiseIPv6(ip: Buffer | Uint8Array) {
     if (!(ip instanceof Uint8Array)) {
-      throw new TypeError(`Provided IP address must be a Buffer or Uint8Array`);
+      throw new TypeError(`IP address must be provided as a Buffer or Uint8Array`);
     }
     if (ip.length !== 16) {
       throw new RangeError(
-        `Provided IP address buffer must be 16 bytes (128 bits) in length, was ${ip.length} bytes`
+        `Provided IPv6 address buffer must be 16 bytes (128 bits) in length, was ${ip.length} bytes`
       );
     }
 
-    throw new Error('Not implemented')
+    return this._pseudonymise(ip);
   }
 
   _pseudonymise(ip: Buffer | Uint8Array) {
+    if (ip.length > 16 || ip.length < 0) {
+      throw new RangeError(
+        `Provided buffer must be between 0 and 16 bytes (128 bits) in length, was ${ip.length} bytes`
+      );
+    }
     // Creates a copy:
     const cipherInput = Buffer.from(this.#padding);
-    // One-time pad, 32-bit buffer initialised with 0s:
-    const otp = Buffer.alloc(4, 0);
+    // One-time pad, initialised with 0s, which will be XORed later:
+    const otp = Buffer.alloc(ip.length, 0);
 
     let output = this.#cipher.update(cipherInput);
     let byteIndex = 0;
     let bitIndex = 0;
     otp[byteIndex] |= (output[0] >>> 7) << 7;
 
-    for (let i = 0; i < 31; ) {  // i is incremented inside the loop
-
+    const iterations = ip.length * 8 - 1;
+    for (let i = 0; i < iterations; ) {  // i is incremented inside the loop body
       const paddingMask = 0xff >>> (bitIndex + 1);
       const ipMask = ~paddingMask;
 
